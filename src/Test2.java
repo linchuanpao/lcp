2b3a9362505aadd63f243ade2988ec836fbc8dae
lcp <13758107486@163.com>
2018-05-13 22:50:52 +0800

    提交linchuanpao

--- src/Test2.java
+++ src/Test2.java
@@ -3,14 +3,15 @@
  */
 public class Test2 {
     public void test1(){
+        //添加linchuanpao
 
     }
     //lcp分支添加内容
     public void test2(){
-
+        //添加linchuanpao
     }
     //linchuanpao添加内容
     public void test3(){
-
+        //添加linchuanpao
     }
 }
